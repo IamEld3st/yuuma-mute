f20f0c64787295a78ef527adfd90c0dc89d2ba37
IamEld3st <iameld3st@gmail.com>
2016-12-31 01:07:11 +0100

    First test of bootstrap part

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // Nothing here yet
+var forever = require('forever-monitor');
+var cfg = require('./config.json');
+
+var mainProcess = new (forever.Monitor)('bot.js', {
+	max: 3,
+	silent: false,
+	args: [],
+	'logFile': cfg.logPath,
+    'outFile': cfg.logPath,
+    'errFile': cfg.logPath
+});
+
+mainProcess.on('exit', function () {
+	console.log('Exited after 3 restarts');
+});
+
+mainProcess.start();
